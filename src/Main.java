7870fc3817b3ce3d5f6a05577901c6d502e591f3
austinatividad <austin_natividad@dlsu.edu.ph>
2023-02-17 16:56:54 +0800

    added obj class and user input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the no. of objects:- ");
+        int count = sc.nextInt();
+
+        ArrayList<obj> List = new ArrayList<>();
+        System.out.println("Enter the weights and profits of each object:");
+        for(int i = 0; i < count; i++){
+            obj object = new obj(i, sc.nextDouble(), sc.nextDouble());
+            List.add(object);
+        }
+
+        for (obj o : List) {
+            System.out.println("Item:" + o.getItem() + " Weight:" + o.getWeight() + " Profits:" + o.getValue() + " Density:" +o.getDensity());
+        }
     }
+
 }
